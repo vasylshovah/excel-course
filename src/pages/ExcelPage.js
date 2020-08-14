8f0d624102e5d03c02a2155bf16c54426118e541
Vasyl Shovag <shovagvs@gmail.com>
2020-08-14 16:17:03 +0300

    Implement DIP

--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -1,6 +1,5 @@
-import {Page} from '@core/Page';
+import {Page} from '@core/page/Page';
 import {createStore} from '@core/store/createStore';
-import {debounce, storage} from '@core/utils';
 import {rootReducer} from '@/redux/rootReducer';
 import {normalizeInitialState} from '@/redux/initialState';
 import {Excel} from '@/components/excel/Excel';
@@ -8,26 +7,25 @@ import {Header} from '@/components/header/Header';
 import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
-
-function storageName(param) {
-    return 'excel:' + param
-}
+import {StateProcessor} from '@core/page/StateProcessor';
+import {LocalStorageClient} from '@/shared/LocalStorageClient';
 
 export class ExcelPage extends Page {
-    getRoot() {
-        console.log(this.params)
-        const params = this.params
-            ? this.params
-            : Date.now().toString()
-        const state = storage(storageName(params))
+    constructor(param) {
+        super(param)
+
+        this.storeSub = null
+        this.processor = new StateProcessor(
+            new LocalStorageClient(this.params)
+        )
+    }
+
+    async getRoot() {
+        const state = await this.processor.get()
         const initialState = normalizeInitialState(state)
         const store = createStore(rootReducer, initialState)
 
-        const stateListener = debounce(state => {
-            storage(storageName(params), state)
-        }, 300)
-
-        store.subscribe(stateListener)
+        this.storeSub = store.subscribe(this.processor.listen)
 
         this.excel = new Excel({
             components: [Header, Toolbar, Formula, Table],
@@ -43,7 +41,6 @@ export class ExcelPage extends Page {
 
     destroy() {
         this.excel.destroy()
+        this.storeSub.unsubscribe()
     }
-    // test() {
-    // }
 }
